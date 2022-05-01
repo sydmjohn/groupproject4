811dfbd966e09a7205d3fd275027d88546c7fbf7
Rose Civitts <99291207+fcivitts@users.noreply.github.com>
2022-05-01 17:07:26 -0400

    Add files via upload

---
+++ GroupAssignment.R
@@ -0,0 +1,39 @@
+credit <- read.csv('credit (1).csv')
+
+credit$month <- 492:1
+credit$month <- yearmonth(credit$month)
+
+credit_ts <- tsibble(credit, index = month)
+
+autoplot(credit_ts)
+gg_season(credit_ts)
+
+
+#Arima
+fit <- credit_ts %>%
+  model(arima1 = ARIMA(ï..credit_in_millions ~ pdq(2,1,0)),
+        arima2 = ARIMA(ï..credit_in_millions ~pdq(0,1,3)),
+        arima3 = ARIMA(ï..credit_in_millions ~pdq(2,0,2)),
+        arima4 = ARIMA(ï..credit_in_millions ~pdq(1,2,1)),
+        arimastepwise = ARIMA(ï..credit_in_millions),
+        searching = ARIMA(ï..credit_in_millions, stepwise = FALSE))
+
+glance(fit) %>% arrange(AICc)
+
+fit <- credit_ts %>% model(ARIMA())
+report(fit)
+
+fit %>% forecast(h=12) %>% autoplot(credit_ts)
+        
+        
+
+
+#Tslm
+
+
+#Naive
+
+
+#Training
+credit_ts <- credit_ts %>%filter(year(month) >= '1970 Feb')
+TrainingCredit <- credit_ts %>% filter(year(month) <= '2004 Jan')
